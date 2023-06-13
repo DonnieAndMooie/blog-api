fa07273e774ade3e9c1b7a8b059528310c51db9f
Owen Eaves <owenjeaves@gmail.com>
2023-06-13 14:28:12 +0100

    Change Date.now() to Date.now

--- models/Blog.js
+++ models/Blog.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const BlogSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now(), required: true },
+  timestamp: { type: Date, default: Date.now, required: true },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   published: { type: Boolean, default: true, required: true },
 });
